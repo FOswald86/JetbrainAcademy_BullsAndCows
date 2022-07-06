7f9e4f368f60584836dfab42ef6df8870d117db6
easy-coding <86290835+FOswald86@users.noreply.github.com>
2022-07-06 13:37:50 +0200

    Add files via upload

---
+++ BullsAndCows/src/at/foswald86/Main.java
@@ -0,0 +1,21 @@
+package at.foswald86;
+
+public class Main {
+    public static void main(String[] args) {
+        String code = Game.createCode();
+        String[] test = code.split("");
+        if (!test[0].equals("E")) {
+            int count = 1;
+            while (true) {
+                System.out.printf("Turn %d:\n", count);
+                if (Game.codeChecker(code)) {
+                    break;
+                }
+                count++;
+            }
+        }
+        else {
+            System.out.println(code);
+        }
+    }
+}
